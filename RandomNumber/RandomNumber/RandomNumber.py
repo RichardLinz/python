84514a8ee06b15217529c496e84be3d3859e78e6
RichardLinz <56728235+RichardLinz@users.noreply.github.com>
2020-08-18 19:16:24 +0200

    Changed RandomNumber, added game function and changed loop

--- RandomNumber/RandomNumber/RandomNumber.py
+++ RandomNumber/RandomNumber/RandomNumber.py
@@ -1,28 +1,37 @@
 import random
+
 # Function random number between 1-100
 def createRandomNumber():
     num = random.randrange(1,100)
     return num
 
-# ----- Console output ------ #
+def game():
+    rnum = createRandomNumber()
+    while True:
+        userinput = input("What is your guess: ")
+
+        # Check the input
+        try:
+            if int(userinput) > rnum:
+                print("Your number is bigger.")
+            elif int(userinput) < rnum:
+                print("Your number is smaller.")
+            elif int(userinput) == rnum: # When input is right
+                print("You guessed right, the number was: " + str(rnum))
+                # Ask to continue game
+                print("Do you want to continue? (write quit to leave)")
+                userinput = input()
+                if userinput == "quit":
+                    print("quitting game")
+                    break
+                else:
+                    rnum = createRandomNumber()
+            elif userinput == "quit":
+                break
+        except:
+            print("Your input is not valid")
+            continue
+
+# Starting game
 print("++ Guess the random number ++")
-userinput = input("What is your guess: ")
-rnum = createRandomNumber()
-# --------------------------- #
-
-# Check the input
-if int(userinput) == rnum:
-    print("Congratulations you guessed right in the first try!!!")
-else:
-    # Loop while input is wrong
-    while int(userinput) != rnum:
-        if int(userinput) > rnum:
-            print("Your number is bigger.")
-        elif int(userinput) < rnum:
-            print("Your number is smaller.")
-        else:
-            print("Your input is not valid.")
-
-        userinput = input("Your next guess: ")
-    # When input is right
-    print("You guessed right, the number was: " + str(rnum))
+game()
