608fedcb0b7902173bbf4017e732db0f330a5ee3
RichardLinz <56728235+RichardLinz@users.noreply.github.com>
2020-08-18 17:41:02 +0200

    Changed RandomNumber to a little game and added a loop

--- RandomNumber/RandomNumber/RandomNumber.py
+++ RandomNumber/RandomNumber/RandomNumber.py
@@ -1,5 +1,28 @@
 import random
+# Function random number between 1-100
+def createRandomNumber():
+    num = random.randrange(1,100)
+    return num
 
-# Random number
-rnum = random.randrange(1,100)
-print(rnum)
+# ----- Console output ------ #
+print("++ Guess the random number ++")
+userinput = input("What is your guess: ")
+rnum = createRandomNumber()
+# --------------------------- #
+
+# Check the input
+if int(userinput) == rnum:
+    print("Congratulations you guessed right in the first try!!!")
+else:
+    # Loop while input is wrong
+    while int(userinput) != rnum:
+        if int(userinput) > rnum:
+            print("Your number is bigger.")
+        elif int(userinput) < rnum:
+            print("Your number is smaller.")
+        else:
+            print("Your input is not valid.")
+
+        userinput = input("Your next guess: ")
+    # When input is right
+    print("You guessed right, the number was: " + str(rnum))
